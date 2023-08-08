ea04a167fb28c82829a34370babbed435c01e4da
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-08 11:06:16 +0530

    contact added

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,18 @@ const productsController = require('../controllers/products');
 
 const router = express.Router();
 
+
+//const contactController=require('../controller/contact')
+//const contactSuccessController=require('../controller/success')
+
 // /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
+
+//router.get('/contact',contactController.getcontact);
+//router.get('/success',contactSuccessController.success );
+//router.post('/contact', contactController.postcontact);
 
 module.exports = router;
